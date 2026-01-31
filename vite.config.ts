199fedc2102aa2a0df9a684b7e19769e19d6c804
RaviTeja799 <bhraviteja799@gmail.com>
2026-01-31 15:24:57 +0530

    Ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/AI-Health-Symptom-Checker/",
   plugins: [
     react(),
     tailwindcss(),
